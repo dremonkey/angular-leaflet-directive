23b1496c371bc7e10b929d3e08fb85f6e2eb2ad6
dremonkey <a.deutmeyer@gmail.com>
2015-02-05 15:09:27 -0800

    wip: mapboxgl directive

--- src/directives/mapboxgl.js
+++ src/directives/mapboxgl.js
@@ -13,19 +13,22 @@ angular.module('leaflet-directive').directive('mapboxgl', function ($log, $rootS
 
       controller.getMap().then(function(map) {
 
-        if (!leafletHelpers.MapboxGLPlugin.isLoaded()) {
-          console.log('Leaflet Mapbox GL Plugin not found...');
-          return;
-        }
+        leafletScope.$watch('mapboxgl', function(mapboxgl) {
 
-        // add if it doesn't exist... 
-        // this will only allow for one mapboxgl "canvas" instance on the page should we allow more?
-        if (!isDefined(leafletMapboxGL) && !map.hasLayer(leafletMapboxGL)) {
-          leafletMapboxGL = L.mapboxGL(mapboxgl);
-          leafletData.setMapboxGL(leafletMapboxGL, attrs.id);
-          leafletMapboxGL.addTo(map);
-          glMap = leafletMapboxGL._glMap;
-        }
+          if (!leafletHelpers.MapboxGLPlugin.isLoaded()) {
+            console.log('Leaflet Mapbox GL Plugin not found...');
+            return;
+          }
+
+          // add if it doesn't exist... 
+          // this will only allow for one mapboxgl "canvas" instance on the page should we allow more?
+          if (!isDefined(leafletMapboxGL) && !map.hasLayer(leafletMapboxGL)) {
+            leafletMapboxGL = L.mapboxGL(mapboxgl);
+            leafletData.setMapboxGL(leafletMapboxGL, attrs.id);
+            leafletMapboxGL.addTo(map);
+            glMap = leafletMapboxGL._glMap;
+          }
+        });
 
         leafletScope.$watch('mapboxgl.layers', function(mapboxgl) {
           // add layers to the the gl map
